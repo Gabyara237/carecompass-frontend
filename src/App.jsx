04120d2d082d715679cc38d5733f80c3f2517c44
Gabriela Araujo <gabyara237@gmail.com>
2026-02-03 01:59:23 -0800

    feat: add landing and dashboard pages with auth-based routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 
+import { useContext } from 'react';
 import { Routes, Route } from 'react-router';
 
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './pages/Landing/Landing';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 
 import './App.css'
 
+import { UserContext } from './contexts/UserContext';
+
 const  App = () => {
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar/>
       <Routes>
+        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
       </Routes>
